a6bdf928406e9d708f70e3fca77d84644fa8abfb
Stan Rodrigues <stan@Stans-MacBook-Air-2.local>
2025-07-27 14:25:19 +0530

    backend

---
+++ environments/prod/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-iac-pipeline"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
